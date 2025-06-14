5b362bf9aae83086578b0d8e5266d31d8e5bacc2
aivandag <aivan.dev7@gmail.com>
2025-06-14 18:43:18 +0000

    modelo de instagram actualizado con tablas follower y media

--- src/models.py
+++ src/models.py
@@ -1,19 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, Boolean, ForeignKey, DateTime, Enum, Integer
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-import datetime, enum
+import datetime
+import enum
 
 db = SQLAlchemy()
-
-class MediaType(enum.Enum):
-     image = 'image'
-     video = 'video'
-     gif = 'gif'
 
 class User(db.Model):
     __tablename__ = 'user'
 
     id: Mapped[int] = mapped_column(primary_key=True)
+    
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     username: Mapped[str] = mapped_column(String(80), unique=True, nullable=False)
@@ -21,33 +18,20 @@ class User(db.Model):
     lastname: Mapped[str] = mapped_column(String(80), nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
-    posts: Mapped[list['Post']] = relationship('Post', back_populates='user', cascade='all, delete-orphan')
-    comments: Mapped[list['Comment']] = relationship('Comment', back_populates='author', cascade='all, delete-orphan')
-    likes: Mapped[list['Like']] = relationship('Like', back_populates='user', cascade='all, delete-orphan')
-    media: Mapped[list['Media']] = relationship('Media', back_populates='post', cascade='all, delete-orphan')
+posts: Mapped[list['Post']] = relationship('Post', back_populates='user', cascade='all, delete-orphan')
+comments: Mapped[list['Comment']] = relationship('Comment', back_populates='user', cascade='all, delete-orphan')
+likes: Mapped[list['Like']] = relationship('Like', back_populates='user', cascade='all, delete-orphan')
 
-    followers: Mapped[list['Follower']] = relationship('Follower', back_populates='followed', cascade='all, delete-orphan')
-    following: Mapped[list['Follower']] = relationship('Follower', back_populates='follower', cascade='all, delete-orphan', foreign_keys='Follower.user_from_id')
+followers: Mapped[list['Follower']] = relationship('Follower', foreign_keys='Follower.user_to_id', back_populates='followed', cascade='all,delete-orphan')
+followings: Mapped[list['Follower']] = relationship('Follower', foreign_keys='Follower.user_from_id', back_populates='follower', cascade='all,delete-orphan')
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            'username': self.username,
-            'firstname': self.firstname,
-            'lastname': self.lastname,
-            "is_active": self.is_active
+def serialize(self):
+    return {
+        "id": self.id,
+        "email": self.email,
+        "is_active": self.is_active
             # do not serialize the password, its a security breach
-        }
-
-class Follower(db.Model):
-     __tablename__ = 'follower'
-     
-     user_from_id: Mapped[int] =mapped_column(ForeignKey('user.id'), primary_key=True)
-     user_to_id: Mapped[int] =mapped_column(ForeignKey('user.id'), primary_key=True)
-
-     follower: Mapped['User'] = relationship('User', foreign_key=[user_from_id], back_populates='following')
-     followed: Mapped['User'] = relationship('User', foreign_key=[user_to_id], back_populates='followers')
+    }
 
 
 class Post(db.Model):
@@ -56,9 +40,10 @@ class Post(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[int]= mapped_column(ForeignKey('user.id'), nullable=False)
 
-user: Mapped[list['User']] = relationship('User', back_populates='posts', cascade='all, delete-orphan')
-comments: Mapped[list['Comment']] = relationship('Comment', back_populates='post', cascade='all, delete-orphan')
-likes: Mapped[list['Like']] = relationship('Like', back_populates='post', cascade='all, delete-orphan')
+    user: Mapped[list['User']] = relationship('User', back_populates='post', cascade='all, delete-orphan')
+    comments: Mapped[list['Comment']] = relationship('Comment', back_populates='post', cascade='all, delete-orphan')
+    likes: Mapped[list['Like']] = relationship('Like', back_populates='post', cascade='all, delete-orphan')
+    media: Mapped[list['Media']] = relationship('Media', back_populates='post', cascade='all, delete-orphan')
 
 def serialize(self):
         return {
@@ -71,39 +56,59 @@ class Comment(db.Model):
 
      id: Mapped[int] = mapped_column(primary_key=True)
      post_id: Mapped[int] = mapped_column(ForeignKey('post.id'), nullable=False)
+     user_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
      text: Mapped[str] = mapped_column(String(350), nullable=False)
-     author_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
 
-     author: Mapped['User'] = relationship('User', back_populates='comments', foreignkey=[author_id])
+     user: Mapped[list['User']] = relationship('User', back_populates='comments', cascade='all, delete-orphan')
      posts: Mapped[list['Post']] = relationship('Post', back_populates='comments', cascade='all, delete-orphan')
 
 def serialize(self):
     return {
         "id": self.id,
         "post_id": self.post_id,
-        "author_id": self.author_id,
+        "user_id": self.user_id,
         "text": self.text,
         }
 
-class Media(db.Model):
-     __tablename__ = 'media'
+class Follower(db.Model):
+     __tablename__= 'follower'
 
-     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-     type: Mapped[Media] = mapped_column(Enum,(Media), nullable=False)
-     url: Mapped[str] = mapped_column(String(350), nullable=False)
+     user_from_id: Mapped[int] = mapped_column(Integer, ForeignKey('user.id'), primary_key=True)
+     user_to_id: Mapped[int] = mapped_column(Integer, ForeignKey('user.id'), primary_key=True)
+
+     follower: Mapped['User'] = relationship('User', foreign_keys=[user_from_id], back_populates='followings')
+     followed: Mapped['User'] = relationship('user', foreign_keys=[user_to_id], back_populates='followers')
+
+     def serialize(self):
+          return{
+               "user_from_id": self.user_from_id,
+               "user_to_id": self.user_to_id
+          }
+     
+class MediaType(enum.Enum):
+          IMAGE = 'image'
+          VIDEO = 'video'
+          GIF = 'gif'
+
+class Media(db.Model):
+     __tablename_= 'media'
+     
+     id: Mapped[int] = mapped_column(primary_key=True)
+     type: Mapped[MediaType] = mapped_column(Enum(MediaType), nullable=False)
+     url: Mapped[str] = mapped_column(String(300), nullable=False)
      post_id: Mapped[int] = mapped_column(ForeignKey('post.id'), nullable=False)
 
-     post: Mapped[['Post']] = relationship('Post', back_populates='media')
+     post: Mapped["Post"] = relationship('Post', back_populates='media')
 
-     
-def serialize(self):
-    return {
-        "id": self.id,
-        "post_id": self.post_id,
-        "type": self.type.value,
-        'url': self.url,
-        }
+     def serialize(self):
+          return{
+               "id": self.id,
+               'type': self.type.value,
+               'url': self.url,
+               'post_id': self.post_id
 
+          }
+    
 
 class Like(db.Model):
      __tablename__ = 'like'
